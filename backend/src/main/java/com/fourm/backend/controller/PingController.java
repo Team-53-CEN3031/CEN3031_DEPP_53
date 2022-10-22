c1d76f18dc9dbddd049ced650ab90e191df3852c
Zachary <tysonzachary@ufl.edu>
2022-10-22 13:37:55 -0400

    Modify PingController

--- backend/src/main/java/com/fourm/backend/controller/PingController.java
+++ backend/src/main/java/com/fourm/backend/controller/PingController.java
@@ -1,12 +1,16 @@
 package com.fourm.backend.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/")
+@CrossOrigin
 public class PingController {
-    @GetMapping("/api")
+    @GetMapping("/ping")
     public ResponseEntity<String> ping () {
         //ping is used to check if the backend is running
         //literally no other use

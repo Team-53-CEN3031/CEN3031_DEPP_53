ae852499b5229e0e8caed494eb63e36d46fdfd27
Zachary <tysonzachary@ufl.edu>
2022-10-23 17:04:15 -0400

    Updated schema for Post entity

--- backend/src/main/java/com/fourm/backend/controller/PostController.java
+++ backend/src/main/java/com/fourm/backend/controller/PostController.java
@@ -1,4 +1,31 @@
 package com.fourm.backend.controller;
 
+import com.fourm.backend.model.Post;
+import com.fourm.backend.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/post")
+@CrossOrigin
 public class PostController {
+    private PostService postService;
+
+    @Autowired
+    public void setPostService(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping("/add")
+    public String add(@RequestBody Post post) {
+        postService.savePost(post);
+        return "New post is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
 }
--- backend/src/main/java/com/fourm/backend/service/PostService.java
+++ backend/src/main/java/com/fourm/backend/service/PostService.java
@@ -1,4 +1,13 @@
 package com.fourm.backend.service;
 
+import com.fourm.backend.model.Post;
+
+import java.util.List;
+
 public interface PostService {
+
+
+    public List<Post> getAllPosts();
+
+    public Post savePost(Post post);
 }

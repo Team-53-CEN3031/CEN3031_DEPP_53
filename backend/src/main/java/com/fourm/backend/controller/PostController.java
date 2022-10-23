3e804abc044777e7ed2be007ca54cb58cb7a8859
Zachary <tysonzachary@ufl.edu>
2022-10-23 17:06:32 -0400

    Fixed request mapping for Posts

--- backend/src/main/java/com/fourm/backend/controller/PostController.java
+++ backend/src/main/java/com/fourm/backend/controller/PostController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/api/post")
 @CrossOrigin
 public class PostController {
     private PostService postService;

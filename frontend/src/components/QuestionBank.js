c7a49f1209f549f6b365d77df781bd48f33c4406
chuerta018 <73916959+chuerta018@users.noreply.github.com>
2022-11-02 09:41:41 -0400

    Update QuestionBank.js
    PLEASE UPDATE THIS BEFORE SPRINT 1 PRESENTATION: Quiz 4 is a question for the team: Its either A and B or C

--- frontend/src/components/QuestionBank.js
+++ frontend/src/components/QuestionBank.js
@@ -8,3 +8,55 @@ export const Questions = [
     "Do you recycle paper?",
     "Do you recycle aluminum and tin ?",
 ]
+
+
+export const AddQuestions = [
+
+    {
+        text: "Are batteries safetly disposable in ....",
+        options: [
+            { id: 0, text: "A: Recycle bin", isCorrect: false },
+            { id: 1, text: "B: Waste Bin", isCorrect: false },
+            { id: 2, text: "Option A and B", isCorrect: false },
+            { id: 3, text: "Neither", isCorrect: true },
+        ],
+    },
+    {
+        text: "Which of the following can you not recycle?",
+        options: [
+            { id: 0, text: "Shredded Paper", isCorrect: true },
+            { id: 1, text: "Cardboard", isCorrect: false },
+            { id: 2, text: "Mail", isCorrect: false },
+            { id: 3, text: "Glass Jars", isCorrect: false },
+        ],
+    },
+    {
+        text: "Which of the following can you recycle?",
+        options: [
+            { id: 0, text: "Crayons", isCorrect: true },
+            { id: 1, text: "Metal", isCorrect: false },
+            { id: 2, text: "Plastic Bottle Caps", isCorrect: false },
+            { id: 3, text: "Rubber", isCorrect: false },
+        ],
+    },
+    {
+        text: "What type of Quiz do you want",
+        options: [
+            { id: 0, text: "How to Recycle quiz", isCorrect: false },
+            { id: 1, text: "Do you recyce correctly", isCorrect: true },
+            { id: 2, text: "How environmental friendly are you?", isCorrect: false },
+            { id: 3, text: "Flappy Bird but with carbon leaving earth instead of bird/tubes", isCorrect: false },
+        ],
+    },
+    {
+        text: "Does Zach Tyson Recycle?",
+        options: [
+            {id: 0, text: "Yes, he's a living role model", isCorrect: false},
+            {id: 1, text: "No, He's a piece of Shit", isCorrect: true},
+            {id: 2, text: "Who is zach?", isCorrect: true},
+            {id: 3, text: "MIKE TYSON SON? Yes", isCorrect: false},
+        ],
+    },
+
+]
+

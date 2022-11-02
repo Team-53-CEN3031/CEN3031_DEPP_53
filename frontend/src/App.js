cad5b808b53e59418b154e8b9290986f5c738ceb
chuerta018 <73916959+chuerta018@users.noreply.github.com>
2022-11-02 09:45:53 -0400

    Update App.js
    Added route for Additonal Quiz

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import Post from "./components/Post";
 import Quiz from "./components/Quiz"
+import AddQuiz from "./components/AddQuiz"
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
                     <Route path="/" element={<Dashboard />} />
                     <Route path="/post" element = {<Post/>}/>
                     <Route path="*" element={<h1>404 Not Found</h1>} />
+                    <Route path="/addquiz" element={<AddQuiz />} />
                 </Routes>
             </BrowserRouter>
         </div>

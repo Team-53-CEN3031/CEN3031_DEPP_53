984a03b7e26e65611b8816476bd012e66f10208f
Zachary <tysonzachary@ufl.edu>
2022-11-04 00:13:39 -0400

    Created page for displaying Leaderboard for quizzes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import Post from "./components/Post";
 import Quiz from "./components/Quiz"
+import Leaderboard from "./components/Leaderboard";
 
 
 function App() {
@@ -16,11 +17,13 @@ function App() {
                     <Route path="/quiz" element={<Quiz />} />
                     <Route path="/" element={<Dashboard />} />
                     <Route path="/post" element = {<Post/>}/>
+                    <Route path="/about" element={<h1>Not made yet</h1>}/>
+                    <Route path="/leaderboard" element={<Leaderboard/>}/>
                     <Route path="*" element={<h1>404 Not Found</h1>} />
                 </Routes>
             </BrowserRouter>
         </div>
-        
+
     );
 }
 

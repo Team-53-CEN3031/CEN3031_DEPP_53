6dcb88ca131ddf0750aa23615c9d187f71d4c9cb
Zachary <tysonzachary@ufl.edu>
2022-11-14 14:21:48 -0500

    Added API function to create comment

--- backend/src/main/java/com/fourm/backend/controller/PostController.java
+++ backend/src/main/java/com/fourm/backend/controller/PostController.java
@@ -1,9 +1,7 @@
 package com.fourm.backend.controller;
 
-import com.fourm.backend.model.Post;
-import com.fourm.backend.model.PostPrototype;
-import com.fourm.backend.model.QuizScore;
-import com.fourm.backend.model.UserPerson;
+import com.fourm.backend.model.*;
+import com.fourm.backend.service.CommentService;
 import com.fourm.backend.service.PostService;
 import com.fourm.backend.service.UserService;
 import com.sun.tools.jconsole.JConsoleContext;
@@ -24,6 +22,13 @@ public class PostController {
     private PostService postService;
 
     private UserService userService;
+
+    private CommentService commentService;
+
+    @Autowired
+    public void setCommentService(CommentService commentService) {
+        this.commentService = commentService;
+    }
 
     @Autowired
     public void setUserService(UserService userService) {
@@ -101,5 +106,60 @@ public class PostController {
             }
         }
         return null;
+    }
+
+    @PostMapping("/postComment/{id}")
+    public ResponseEntity<?> postComment(@PathVariable String id,@RequestBody CommentPrototype commentPrototype) {
+        //Try to convert the id to an int
+        int postId;
+        try {
+            postId = Integer.parseInt(id);
+        } catch (NumberFormatException e) {
+            return new ResponseEntity<>("Invalid post id", HttpStatus.BAD_REQUEST);
+        }
+
+        //Check if the commenter token is null
+        String commenterToken = commentPrototype.getCommenterToken();
+        if(commenterToken == null) {
+            return new ResponseEntity<>("Commenter token is null", HttpStatus.BAD_REQUEST);
+        }
+
+        //Verify the token
+        if(authController.validateJwtToken(commenterToken).getStatusCode() != HttpStatus.OK){
+            return new ResponseEntity<>("Invalid token", HttpStatus.UNAUTHORIZED);
+        }
+
+
+        String commentMessage = commentPrototype.getCommentMessage();
+        if(commentMessage == null) {
+            return new ResponseEntity<>("Comment message is null", HttpStatus.BAD_REQUEST);
+        }
+        if(commentMessage.length() > 1000) {
+            return new ResponseEntity<>("Comment message is too long", HttpStatus.BAD_REQUEST);
+        }
+        if(commentMessage.length() < 1) {
+            return new ResponseEntity<>("Comment message is too short", HttpStatus.BAD_REQUEST);
+        }
+
+        //Get the commenter
+        UserPerson commenter = authController.getUserFromToken(commenterToken);
+        if(commenter == null) {
+            return new ResponseEntity<>("Commenter does not exist", HttpStatus.BAD_REQUEST);
+        }
+
+        //Check if the post exists
+        Post post = getPost(postId);
+        if(post == null) {
+            return new ResponseEntity<>("Post does not exist", HttpStatus.BAD_REQUEST);
+        }
+
+        //Create the comment
+        Comment comment = new Comment();
+        comment.setCommentMessage(commentMessage);
+        comment.setCommentTime(new java.sql.Timestamp(System.currentTimeMillis()));
+        comment.setPost(post);
+        comment.setUser(commenter);
+        commentService.saveComment(comment);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }

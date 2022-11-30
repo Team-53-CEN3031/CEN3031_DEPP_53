60c1b21f3dfe6f4c242c0d682caf4d31c0e8ca98
Zachary <tysonzachary@ufl.edu>
2022-11-30 15:43:02 -0500

    Created component to send emails

---
+++ backend/src/main/java/com/fourm/backend/service/EmailServiceImpl.java
@@ -0,0 +1,28 @@
+package com.fourm.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailServiceImpl{
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    @Async
+    public void sendSimpleEmail(String to, String subject, String body) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(from);
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(body);
+        javaMailSender.send(simpleMailMessage);
+    }
+}
+
